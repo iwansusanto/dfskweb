d49a97593ea8aca4e748319300cc7648fccc321d
Iwan Susanto <iwandevapps@gmail.com>
2019-04-15 10:57:16 +0700

    add menu buttton on mobile

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -18,7 +18,7 @@
         aria-controls="navbarNavCollapse"
         aria-expanded="false" 
         aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <i class="fa fa-bars"></i>
     </button>
 
     <!-- Navbar links -->
